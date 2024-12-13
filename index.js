c18e6bb269b6cd506df7c59e1707dd6b286066c4
paulantony066 <paulantony066@gmail.com>
2024-12-13 13:17:51 +0530

    Fix: Added cors

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ $env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\raphyantony\notify\familyevent-934
 import {initializeApp, applicationDefault } from 'firebase-admin/app';
 import { getMessaging } from "firebase-admin/messaging";
 import express, { json } from "express";
+import cors from "cors";
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const app = express();
