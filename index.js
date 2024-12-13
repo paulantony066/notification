df43f0e62c1737373693f1882efcfaf7fcdd8171
paulantony066 <paulantony066@gmail.com>
2024-12-13 13:06:30 +0530

    Added firebase-admin to dependencies

--- index.js
+++ index.js
@@ -12,7 +12,62 @@ import {initializeApp, applicationDefault } from 'firebase-admin/app';
 import { getMessaging } from "firebase-admin/messaging";
 import express, { json } from "express";
 
+process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const app = express();
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.use(
+    cors({
+        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    })
+);
+
+app.use(function(req, res, next){
+    res,setHeader("Content_Type","application/json");
+    next();
+});
+
+
 initializeApp({
     Credential: applicationDefault(),
     projectId: 'familyevent-934de',
 });
+
+app.post("/send", function(req, res){
+
+    const recievedToken = req.body.fcmToken;
+    const message = {
+        notification: {
+          title: 'notify',
+          body: 'testttttt'
+        },
+        token: recievedToken
+    };
+
+    getMessaging()
+        .send(message)
+        .then((response)=>{
+            res.status(200).json({
+                message: "successs",
+                token: recievedToken,
+            });
+            console.log("message sent success:", response);
+        })
+        .catch((error)=>{
+            res.status(400);
+            res.send(error);
+            console.log("error sending message:", error);
+        });
+
+
+});
+
+app.listen(3000, function(){
+    console.log("server started on 3000");
+})
